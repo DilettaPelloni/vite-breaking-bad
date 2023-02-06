7b00b0ee6690e8d6d8bde1c0c9d35eab85f43602
Diletta Pelloni <dilpel@live.it>
2023-02-06 15:27:45 +0100

    installato bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
   import AppHeader from './components/AppHeader.vue';
   import AppMain from './components/AppMain.vue';
-  
+
   export default {
     name:'App',
     components: {
@@ -12,8 +12,11 @@
 </script>
 
 <template>
+  <h1>Hello World!</h1>
   <AppHeader/>
 </template>
 
 <style  lang="scss">
+	@import "./style/main";
+
 </style>
