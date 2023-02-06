077a7c06ff56b53ea6dddf75c454667c3c97ad9d
Diletta Pelloni <dilpel@live.it>
2023-02-06 16:08:36 +0100

    sovrascritto colore primary

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-    <main class="py-4">
+    <main class="py-4 bg-primary">
       <div class="container">
         <section class="mb-4">
           <select name="filter" >
@@ -19,7 +19,7 @@
             <p class="m-0">Found x cards</p>
           </div><!-- CHIUSURA ROW DI TESTA -->
           <div class="row row-cols-5">
-            <div class="col my-card p-0 text-center">
+            <div class="col my-card p-0 bg-primary text-center">
               <img class="img-fluid" src="https://i.ebayimg.com/images/g/Y-kAAOSw2ppg7dl3/s-l500.jpg" alt="prova">
               <p class="my-3 text-light fw-bold">EXODIA</p>
               <p class="m-0">alieno</p>
@@ -31,11 +31,4 @@
 </template>
 
 <style  lang="scss" scoped>
-main {
-  background-color: $main-orange;
-}
-
-.my-card {
-  background-color: $main-orange;
-}
 </style>
