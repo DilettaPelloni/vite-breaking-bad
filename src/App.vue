36735b7984f7218123dd72f46b82c69d662fa8e0
Diletta Pelloni <dilpel@live.it>
2023-02-06 17:35:21 +0100

    esercizio completato

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <script>
-  import AppHeader from './components/AppHeader.vue';
-  import AppMain from './components/AppMain.vue';
+    import AppHeader from './components/AppHeader.vue';
+    import AppMain from './components/AppMain.vue';
+    import axios from 'axios';
+    import {store} from './store';
 
-  export default {
-    name:'App',
-    components: {
-      AppHeader,
-      AppMain,
-    },//components
-  };//export
+    export default {
+        name:'App',
+            components: {
+            AppHeader,
+            AppMain,
+        },//components
+        data() {
+            return {
+                store,
+            };//return
+        },//data
+        created() {
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php').then((response) => {
+                this.store.cards = response.data.data.slice(0,20);
+            });
+        },//created
+    };//export
 </script>
 
 <template>
-  <AppHeader/>
-  <AppMain/>
+    <AppHeader/>
+    <AppMain/>
 </template>
 
 <style  lang="scss">
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,32 +1,41 @@
 <script>
-  export default {
-    name:'AppMain',
-  }
+import {store} from '../store';
+    export default {
+        name:'AppMain',
+        data() {
+            return {
+                store,
+            };//return
+        },//data
+    };//export
 </script>
 
 <template>
     <main class="py-4 bg-primary">
-      <div class="container">
-        <section class="mb-4">
-          <select name="filter" >
-            <option value="Alien" selected>
-              Alien
-            </option>
-          </select>
-        </section><!-- CHIUSURA SEZIONE SELECT -->
-        <section class="p-5 bg-light">
-          <div class="row py-2 bg-dark text-light fw-bold">
-            <p class="m-0">Found x cards</p>
-          </div><!-- CHIUSURA ROW DI TESTA -->
-          <div class="row row-cols-5">
-            <div class="col my-card p-0 bg-primary text-center">
-              <img class="img-fluid" src="https://i.ebayimg.com/images/g/Y-kAAOSw2ppg7dl3/s-l500.jpg" alt="prova">
-              <p class="my-3 text-light fw-bold">EXODIA</p>
-              <p class="m-0">alieno</p>
-            </div>
-          </div>
-        </section><!-- CHIUSURA SEZIONE CARD BOX -->
-      </div><!-- CHIUSURA CONTAINER -->
+        <div class="container">
+            <section class="mb-4">
+                <select name="filter" >
+                    <option value="Alien" selected>
+                        Alien
+                    </option>
+                </select>
+            </section><!-- CHIUSURA SEZIONE SELECT -->
+            <section class="p-5 bg-light">
+                <div class="row bg-dark text-light fw-bold ">
+                        <p class="m-0 py-2 ">Found {{store.cards.length}} cards</p>
+                </div><!-- CHIUSURA ROW DI TESTA -->
+                <div class="row row-cols-5 justify-content-between g-3">
+                    <div class="col text-center"
+                        v-for="card in store.cards">
+                        <div class="my-card h-100 bg-primary ">
+                            <img class="img-fluid" :src="card.card_images[0].image_url_small" :alt="card.name">
+                            <p class="my-3 text-light fw-bold">{{card.name}}</p>
+                            <p class="m-0">{{card.type}}</p>
+                        </div><!-- CHIUSURA CARD -->
+                    </div><!-- CHIUSURA COL CARD -->
+                </div><!-- CHIUSURA ROW CARDS -->
+            </section><!-- CHIUSURA SEZIONE CARD BOX -->
+        </div><!-- CHIUSURA CONTAINER -->
     </main>
 </template>
 
