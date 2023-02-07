73a1bb3c02bbdfb3c4ed59e92af71075d52ec8d0
Diletta Pelloni <dilpel@live.it>
2023-02-07 14:44:22 +0100

    completata milestone2

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,6 +7,7 @@ import axios from 'axios';
         data() {
             return {
                 store,
+                archetypeFilter: '',
             };//return
         },//data
         methods: {
@@ -21,6 +22,20 @@ import axios from 'axios';
                     this.store.archetypes = response.data;
                 });
             },//getArchetypes
+            filterCards() {
+                if (this.archetypeFilter != '') {
+                    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php', {
+                        params: {
+                            archetype: this.archetypeFilter,
+                        }
+                    }).then((response) => {
+                        this.store.cards = response.data.data.slice(0,50);
+                    });
+                }
+                else {
+                    this.getCards();
+                }
+            },//filetrCards
         },//methods
         created() {
             this.getCards();
@@ -35,7 +50,7 @@ import axios from 'axios';
 
             <section class="my-content" v-if="store.loaded">
                 <section class="mb-4">
-                    <select class="form-select w-25" name="filter" >
+                    <select class="form-select w-25" name="filter" v-model="archetypeFilter" @change="filterCards()">
                         <option value="" selected> Select an archetype</option>
                         <option v-for="archetype in store.archetypes" :value="archetype.archetype_name">
                             {{ archetype.archetype_name }}
@@ -46,10 +61,10 @@ import axios from 'axios';
                     <div class="row bg-dark text-light fw-bold ">
                             <p class="m-0 py-2 ">Found {{store.cards.length}} cards</p>
                     </div><!-- CHIUSURA ROW DI TESTA -->
-                    <div class="row row-cols-5 justify-content-between g-3">
+                    <div class="row row-cols-5 g-3">
                         <div class="col text-center"
                             v-for="card in store.cards">
-                            <div class="my-card h-100 px-2 bg-primary ">
+                            <div class="my-card h-100 p-2 bg-primary ">
                                 <img class="img-fluid" src="https://via.placeholder.com/168x246" :alt="card.name">
                                 <p class="my-3 text-light fw-bold">{{card.name}}</p>
                                 <p class="m-0">{{card.type}}</p>
